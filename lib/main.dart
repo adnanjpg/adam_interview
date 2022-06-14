16e37aeab789ecdce6f74c7c50084c16338a4a3f
Adnan <adnanjpg@github.com>
2022-06-14 22:45:29 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const primary = Color(0xFF53BF9D);
+
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: primary,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.light,
+          secondary: Color(0xFFF94C66),
+          onSecondary: Colors.black,
+          background: Color(0xFFFFC54D),
+          onBackground: Colors.black,
+          error: Colors.red,
+          onError: Colors.white,
+          surface: Color(0xFFBD4291),
+          onSurface: Color(0xFFFFFFFF),
+          primary: primary,
+          onPrimary: Colors.black,
+        ),
+      ),
+    );
+  }
+}
